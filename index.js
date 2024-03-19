7d501daca099f0725ee49023829315f0925dee6c
Stephen Ochieng <materiamystic@gmail.com>
2024-03-20 00:42:52 +0300

    Adds required functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun (activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;   
+}
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;             
+}
+
+function wrapAdjective (j="*") {
+    return function (p = "special") {
+        return `You are ${j}${p}${j}!`;
+    }
+}
